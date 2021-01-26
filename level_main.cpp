1cd8ea126c0cd7f09025ef1712fb81fdccb00c9c
Brett Chiu <72076153+brettc-git@users.noreply.github.com>
2021-01-26 14:42:15 -0800

    Update level_main.cpp

--- level_main.cpp
+++ level_main.cpp
@@ -1,7 +1,5 @@
 /***************************
-
 Level-Based Method
-
 ***************************/
 
 #include <iostream>
@@ -17,61 +15,86 @@ using std::vector;
 
 // If the player gets an answer wrong, gameOver will be set to true, thus ending the game.
 
-void HigherLowerGame(vector<Card> set, bool &gameOver_status)
+void HigherLowerGame(vector<Card> set, bool& gameOver_status)
 {
 	char choice;
 
 	vector<string> mystery_set(set.size());		// Create a string vector that will mask the set's cards (except for one) with question marks.
-	fill(mystery_set.begin()+1, mystery_set.end(), "?");  // Hidden answers 
+	fill(mystery_set.begin() + 1, mystery_set.end(), "?");  // Hidden answers 
 
 	mystery_set[0] = set[0].printCard();
 
-	for (int i = 0; i < (set.size()-1) && gameOver_status != true; i++) {  // Display hidden answers
-			
-			for (auto it : mystery_set) // ? ? ? ? ? ? ? -> D-6 ? ? ? ? ? -> D-6 C-5 ? ? ? ? 
-			cout << it << " "; 
+	for (int i = 0; i < (set.size() - 1) && gameOver_status != true; i++) {  // Display hidden answers
 
-			cout << "Higher or Lower? (H/L)" << endl;
-			cin >> choice;
+		for (auto it : mystery_set) // ? ? ? ? ? ? ? -> D-6 ? ? ? ? ? -> D-6 C-5 ? ? ? ? 
+			cout << it << " ";
 
-			switch (choice) {
-			case 'H':
-			case 'h':
-				if (set[i + 1].getFace() >= set[i].getFace())
-					mystery_set[i + 1] = set[i + 1].printCard();
-				else {
-					gameOver_status = true;
-					cout << "Losing Card: " << set[i + 1].printCard() << endl;
+		cout << "Higher or Lower? (H/L)" << endl;
+		cin >> choice;
 
-					cout << "Answers: " << endl;
-					for (auto it_d : set)
-						cout << it_d.printCard() << " ";
-				 
-				}
-				break;
-			case 'L':
-			case 'l':
-				if (set[i + 1].getFace() <= set[i].getFace()) {
-					mystery_set[i + 1] = set[i + 1].printCard();
-				}
-				else {
-					gameOver_status = true;
-					cout << "Losing Card: " << set[i + 1].printCard() << endl;
+		switch (choice) {
+		case 'H':
+		case 'h':
+			if (set[i + 1].getFace() >= set[i].getFace())
+				mystery_set[i + 1] = set[i + 1].printCard();
+			else {
+				gameOver_status = true;
+				cout << "Losing Card: " << set[i + 1].printCard() << endl;
 
-					cout << "Answers: " << endl;
-					for (auto it_d : set)
-						cout << it_d.printCard() << " ";
-				}
-					break;
-			default:
-				cout << "Invalid choice occurred." << endl;
+				cout << "Answers: " << endl;
+				for (auto it_d : set)
+					cout << it_d.printCard() << " ";
+
 			}
+			break;
+		case 'L':
+		case 'l':
+			if (set[i + 1].getFace() <= set[i].getFace()) {
+				mystery_set[i + 1] = set[i + 1].printCard();
+			}
+			else {
+				gameOver_status = true;
+				cout << "Losing Card: " << set[i + 1].printCard() << endl;
+
+				cout << "Answers: " << endl;
+				for (auto it_d : set)
+					cout << it_d.printCard() << " ";
+			}
+			break;
+		default:
+			cout << "Invalid choice occurred." << endl;
+		}
 
 	}
 
 	if (gameOver_status != true) {
-		cout << "You win!" << endl;
+		cout << "You win this round!" << endl;
 		for (auto it_v : mystery_set)
 			cout << it_v << " ";
 	}
+}
+
+int main()
+{
+	bool gameOver = false;
+	card_deck game_;
+	int amount_keys[4] = { 7, 15, 26, 39 };
+	string difficulty_keys[4] = { "Easy", "Medium", "Hard", "Insane" };
+
+	for (int i = 0; i < 4 && gameOver != true; i++) { 
+		game_.shuffleDeck();
+		vector<Card> set(amount_keys[i]); 
+
+		set = game_.dealCards(set);
+
+		cout << "\n";
+
+		cout << difficulty_keys[i] << " Mode:" << endl;
+		HigherLowerGame(set, gameOver);
+	}
+
+	if (gameOver != true)
+		cout << "Congratulations! You won the game!" << endl;
+
+	return 0;
 }
